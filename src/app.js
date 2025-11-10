7c0ea29786bc1ec47f3e9ab156b483464e0f41e6
LuisOlmos12 <luisaurelioolmosreyes2@gmail.com>
2025-11-10 13:31:25 -0600

    8 commit

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ app.post('/removeFavorito', (req, res) => {
 
 
 
-// Servidor
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
